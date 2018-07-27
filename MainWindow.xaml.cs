d66af49c6817b7eab4cf74478891966948ba6977
Saghen <saghendev@gmail.com>
2018-07-27 17:18:00 -0400

    Base Functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 using Newtonsoft.Json;
-using RVDL;
 
 namespace RVDL
 {
@@ -41,15 +36,52 @@ namespace RVDL
                 Failed("The domain of the url was not found to be www.reddit.com");
                 return;
             }
-            string json;
+
+            DataSchema.Reddit_Video data;
+            try
+            {
+                string json;
+                using (WebClient wc = new WebClient())
+                {
+                    json = wc.DownloadString(url + ".json");
+                }
+                data = JsonConvert.DeserializeObject<List<DataSchema>>(json)[0].data.children[0].data.media.reddit_video;
+            }
+            catch (Exception ex)
+            {
+                Failed("The URL provided is not of a valid post");
+                return;
+            }
+            string videoPath = Path.GetTempFileName() + ".mp4";
+            string audioPath = Path.GetTempFileName() + ".mp3";
+
             using (WebClient wc = new WebClient())
             {
-                json = wc.DownloadString(url + ".json");
+                wc.DownloadFile(data.fallback_url, videoPath);
+                wc.DownloadFile(new Uri(new Uri(data.fallback_url), ".") + "audio", audioPath);
             }
-            var data = JsonConvert.DeserializeObject<List<DataSchema>>(json)[0].data.children[0].data.media.reddit_video;
-        }
+            string ffmpeg = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + "\\ffmpeg\\ffmpeg.exe";
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "output";
+            dlg.DefaultExt = "mp4";
+            dlg.Filter = "MPEG Files (*.mp4)|*.mp4";
 
-        
+            dlg.InitialDirectory = Properties.Settings.Default.Path;
+            if (dlg.InitialDirectory == "")
+                dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+
+            if (!(bool)dlg.ShowDialog())
+            {
+                Failed("Save picker was closed");
+                return;
+            }
+                
+
+            Properties.Settings.Default.Path = Path.GetDirectoryName(dlg.FileName);
+            Properties.Settings.Default.Save();
+
+            var result = Process.Start(ffmpeg, "-i " + videoPath + " -i " + audioPath + " -c copy " + dlg.FileName);
+        }
 
         private void Failed(string err)
         {
