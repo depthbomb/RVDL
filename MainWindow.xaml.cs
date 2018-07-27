40b17a9ecee19450562d7a11f603f480e209db7c
Saghen <saghendev@gmail.com>
2018-07-27 01:01:04 -0400

    Initial Program Structure

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using RVDL;
 
 namespace RVDL
 {
@@ -23,6 +26,35 @@ namespace RVDL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            if (!Uri.IsWellFormedUriString(txtBoxURL.Text, UriKind.Absolute))
+            {
+                Failed("The URL provided is not valid");
+                return;
+            }
+            var url = new Uri(txtBoxURL.Text);
+            if (url.GetLeftPart(UriPartial.Authority).Substring(url.GetLeftPart(UriPartial.Scheme).Length) != "www.reddit.com")
+            {
+                Failed("The domain of the url was not found to be www.reddit.com");
+                return;
+            }
+            string json;
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(url + ".json");
+            }
+            var data = JsonConvert.DeserializeObject<List<DataSchema>>(json)[0].data.children[0].data.media.reddit_video;
+        }
+
+        
+
+        private void Failed(string err)
+        {
+            txtError.Text = err;
+            txtError.Visibility = Visibility.Visible;
         }
     }
 }
