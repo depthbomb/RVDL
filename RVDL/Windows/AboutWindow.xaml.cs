5a97fc846370f3ba3fe35783e04e726ae2c8057a
depthbomb <reesepup77@hotmail.com>
2019-11-25 18:08:38 -0600

    V2 overhaul

---
+++ RVDL/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace RVDL.Windows
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            this.AppVersion.Text = Version.FullVersion;
+
+        }
+
+        private void DepthbombLink(object sender, RoutedEventArgs e)
+            => OpenUrl("https://github.com/depthbomb");
+
+        private void SaghenLink(object sender, RoutedEventArgs e)
+            => OpenUrl("https://github.com/saghen");
+
+        private void FfmpegLink(object sender, RoutedEventArgs e)
+            => OpenUrl("https://www.ffmpeg.org/");
+
+        private void OpenUrl(string url)
+            => System.Diagnostics.Process.Start("explorer.exe", url);
+    }
+}
